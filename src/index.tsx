43fe1534f08414bddc8e967ef040c52e5a339c43
Bilhalva <pedrokabilhalva@gmail.com>
2023-10-04 12:02:50 -0300

    +fonte

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './input.css';
-import App from './pages/App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import ReactDOM from "react-dom/client";
+import "./input.css";
+import App from "./pages/App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <h1 className="font-bold">
-      Simple React Typescript Tailwind Sample
-    </h1>
-  );  
+      <h1 className="font-bold">Simple React Typescript Tailwind Sample</h1>
+  );
 }
 
 export default App;
