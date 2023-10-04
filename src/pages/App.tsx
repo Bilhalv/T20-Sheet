1fd9bb63d84df2a16c111a623f5e5fde598d931f
Bilhalva <pedrokabilhalva@gmail.com>
2023-10-04 11:17:04 -0300

    Folder organization update

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
+    <h1 className="font-bold">
       Simple React Typescript Tailwind Sample
     </h1>
   );  
