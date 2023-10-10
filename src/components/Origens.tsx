e6630da3be79bbc51181456cec9c12910b6423e5
Senac-Bil <pedrokabilhalva@gmail.com>
2023-10-10 10:48:01 -0300

    Origens fix

--- src/components/Origens.tsx
+++ src/components/Origens.tsx
@@ -3,55 +3,46 @@ import React, { useState } from "react";
 import { TabelaOrigens } from "../classes/TabelaOrigens";
 
 const Origens: React.FC = () => {
-  const [origem, setOrigem] = useState(TabelaOrigens[0].nome);
-  const [origemDesc, setOrigemDesc] = useState(TabelaOrigens[0].descricao
-  );
-  const [origemItens, setOrigemItens] = useState(TabelaOrigens[0].itens);
-  const [origemBeneficios, setOrigemBeneficios] = useState(TabelaOrigens[0].beneficios);
-  const [origemImage, setOrigemImage] = useState(TabelaOrigens[0].imagem);
-  const [origemPoder, setOrigemPoder] = useState(TabelaOrigens[0].poder);
-  
+  const [origem, setOrigem] = useState(TabelaOrigens[0]);
+
+  const handleOrigemChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedOrigem = TabelaOrigens.find((origem) => origem.nome === event.target.value);
+    if (selectedOrigem) {
+      setOrigem(selectedOrigem);
+    }
+  }
   return (
     <>
       <h1 className="text-xl text-center mb-4">Origem</h1>
       <section className="bg-gray-300 p-3 rounded-lg bg-opacity-80 shadow-lg h-fit">
         <section className="flex flex-col desktop:flex-row gap-2">
           <div className="desktop:w-[50%]">
-            <Select placeholder="Escolha Sua Origem">
+            <Select
+              placeholder="Escolha Sua Origem"
+              onChange={handleOrigemChange}
+            >
               {TabelaOrigens.map((origem) => (
-                <option
-                  value={origem.nome}
-                  onClick={() => {
-                    
-                    setOrigem(origem.nome);
-                    console.log("Selected Origin: ",origem.nome);
-                    setOrigemDesc(origem.descricao);
-                    setOrigemItens(origem.itens);
-                    setOrigemBeneficios(origem.beneficios);
-                    setOrigemImage(origem.imagem);
-                    setOrigemPoder(origem.poder);
-                  }}
-                >
+                <option key={origem.nome} value={origem.nome}>
                   {origem.nome}
                 </option>
               ))}
             </Select>
-            <img className="mx-auto" src={origemImage} />
+            <img className="mx-auto" src={origem.imagem} alt={origem.nome} />
           </div>
           <div className="desktop:w-[50%]">
-            <h1 className="text-center text-2xl mb-2">{origem}</h1>
-            <p className="text-sm my-auto">&nbsp;&nbsp;{origemDesc}</p>
+            <h1 className="text-center text-2xl mb-2">{origem.nome}</h1>
+            <p className="text-sm my-auto">&nbsp;&nbsp;{origem.descricao}</p>
             <ul className="mt-2">
               <li>
-                <b>Itens.</b> {origemItens.join(", ")}
+                <b>Itens.</b> {origem.itens.join(", ")}
               </li>
               <li>
-                <b>Benefícios.</b> {origemBeneficios.pericias.join(", ")}
-                (perícias) e {origemBeneficios.poderes.join(", ")} (poderes).
+                <b>Benefícios.</b> {origem.beneficios.pericias.join(", ")}
+                (perícias) e {origem.beneficios.poderes.join(", ")} (poderes).
               </li>
             </ul>
-            <h3 className="mt-2 font-bold">{origemPoder.nome}</h3>
-            <p>{origemPoder.descricao}</p>
+            <h3 className="mt-2 font-bold">{origem.poder.nome}</h3>
+            <p>{origem.poder.descricao}</p>
           </div>
         </section>
         <button className="my-2 text-red-800 bg-white hover:bg-gray-300 px-5 rounded w-full transition-all ease-in-out shadow-lg">
--- src/components/Racas.tsx
+++ src/components/Racas.tsx
@@ -17,24 +17,14 @@ const RaceButton: React.FC<RaceButtonProps> = ({ raceName, onClick }) => {
   );
 };
 
-const racas = ["Humano", "Anão", "Dahllan", "Elfo", "Goblin", "Lefou", "Minotauro", "Qareen", "Golem", "Hynne", "Kliren", "Medusa", "Osteon", "Sereia/Tritão", "Sílfide", "Suraggel (Aggelus)", "Suraggel (Sulfure)", "Trog"]
-
 const Racas: React.FC = () => {
-  const [race, setRace] = useState(TabelaRacas[0].nome);
-  const [raceDescription, setRaceDescription] = useState(TabelaRacas[0].descricao);
-  const [racePowers, setRacePowers] = useState(TabelaRacas[0].poderes);
-  const [atribute, setAtribute] = useState(TabelaRacas[0].atributos);
-  const [raceImage, setRaceImage] = useState(TabelaRacas[0].imagem);
+  const [race, setRace] = useState(TabelaRacas[0]);
 
   const handleClick = (raceName: string) => {
     console.log(`Selected race: ${raceName}`);
-    setRace(raceName);
     const selectedRace = TabelaRacas.find((race) => race.nome === raceName);
     if (selectedRace) {
-      setRaceDescription(selectedRace.descricao);
-      setAtribute(selectedRace.atributos);
-      setRacePowers(selectedRace.poderes);
-      setRaceImage(selectedRace.imagem);
+      setRace(selectedRace);
     }
   };
 
@@ -43,14 +33,14 @@ const Racas: React.FC = () => {
       <h1 className="text-xl text-center mb-4">Raça</h1>
       <div className="flex flex-col desktop:flex-row gap-4 w-full">
         <section className="bg-gray-300 flex flex-col p-3 rounded-lg bg-opacity-80 shadow-lg h-fit desktop:w-[50%] w-full">
-          <h1 className="text-center text-2xl mb-2">{race}</h1>
+          <h1 className="text-center text-2xl mb-2">{race.nome}</h1>
           <div className="flex">
-            <img src={raceImage} className="w-[50%] h-fit" alt="Personagem" />
-            <p className="text-sm my-auto">{raceDescription}</p>
+            <img src={race.imagem} className="w-[50%] h-fit" alt="Personagem" />
+            <p className="text-sm my-auto">{race.descricao}</p>
           </div>
-          <h2 className="text-center text-xl my-2">{atribute.join(" | ")}</h2>
+          <h2 className="text-center text-xl my-2">{race.atributos.join(" | ")}</h2>
           <ul>
-            {racePowers.map((power, index) => (
+            {race.poderes.map((power, index) => (
               <li key={index} className="text-sm my-2">
                 <i>
                   <b className="font-bold">{power.nome}</b> {power.descricao}{" "}
@@ -62,9 +52,13 @@ const Racas: React.FC = () => {
             Confirmar
           </button>
         </section>
-        <section className="grid grid-cols-3 gap-5 mx-auto h-fit">
-          {racas.map((raca) => (
-            <RaceButton raceName={raca} onClick={() => handleClick(raca)}/>
+        <section className="grid grid-cols-3 gap-5 mx-auto h-fit my-auto">
+          {TabelaRacas.map((raca) => (
+            <RaceButton
+              key={raca.nome}
+              raceName={raca.nome}
+              onClick={() => handleClick(raca.nome)}
+            />
           ))}
         </section>
       </div>
