c859183745f9cd58b9145cb3f21a49f6d4871e92
Diego Nachtigall <ditsguts@gmail.com>
2023-10-18 15:57:42 -0300

    + Todas pericias

--- src/classes/Tabelas/Pericias.ts
+++ src/classes/Tabelas/Pericias.ts
@@ -37,7 +37,176 @@ const TabelaPericias: Pericia[] = [
         somentetreinada: false,
         penalidadearmadura: false
     },
+    {
+        nome: "Conhecimento",
+        descricao: "Você estudou diversos campos do saber, como aritmética, astronomia, dialética, geografia, história...",
+        atributo: Atributo.inteligencia,
+        somentetreinada: true,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Cura",
+        descricao: "Você sabe tratar ferimentos, doenças e venenos.",
+        atributo: Atributo.sabedoria,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Diplomacia",
+        descricao: "Você convence pessoas com lábia e argumentação. Usos de Diplomacia são efeitos mentais.",
+        atributo: Atributo.carisma,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Enganação",
+        descricao: "Você manipula pessoas com blefes e trapaças.",
+        atributo: Atributo.carisma,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Fortitude",
+        descricao: "Você usa esta perícia para resistir a efeitos que exigem vitalidade, como doenças e venenos.",
+        atributo: Atributo.constituicao,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Furtividade",
+        descricao: "Você sabe ser discreto e sorrateiro.",
+        atributo: Atributo.destreza,
+        somentetreinada: false,
+        penalidadearmadura: true
+    },
+    {
+        nome: "Guerra",
+        descricao: "Você foi educado em tática, estratégia e logística.",
+        atributo: Atributo.inteligencia,
+        somentetreinada: true,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Iniciativa",
+        descricao: "Esta perícia determina sua velocidade de reação em situações de perigo.",
+        atributo: Atributo.destreza,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Intimidação",
+        descricao: "Você pode assustar ou coagir outras pessoas. Usos de Intimidação são efeitos de medo.",
+        atributo: Atributo.carisma,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Intuição",
+        descricao: "Esta perícia mede sua empatia e “sexto sentido”.",
+        atributo: Atributo.sabedoria,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Investigação",
+        descricao: "Você sabe encontrar pistas e informações.",
+        atributo: Atributo.inteligencia,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Jogatina",
+        descricao: "Você sabe jogar jogos de azar.",
+        atributo: Atributo.carisma,
+        somentetreinada: true,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Ladinagem",
+        descricao: "Você sabe exercer atividades ilícitas.",
+        atributo: Atributo.destreza,
+        somentetreinada: true,
+        penalidadearmadura: true
+    },
+    {
+        nome: "Luta",
+        descricao: "Você usa Luta para fazer ataques corpo a corpo.",
+        atributo: Atributo.forca,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Misticismo",
+        descricao: "Esta perícia envolve o conhecimento de magias, itens mágicos e fenômenos sobrenaturais.",
+        atributo: Atributo.inteligencia,
+        somentetreinada: true,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Nobreza",
+        descricao: "Você recebeu a educação de um nobre.",
+        atributo: Atributo.inteligencia,
+        somentetreinada: true,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Ofício",
+        descricao: "Ofício na verdade são várias perícias diferentes. Cada uma permite fabricar itens de certas categorias",
+        atributo: Atributo.inteligencia,
+        somentetreinada: true,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Percepção",
+        descricao: "Você nota coisas usando seus sentidos.",
+        atributo: Atributo.sabedoria,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Pilotagem",
+        descricao: "Você sabe operar veículos como carroças, barcos e balões.",
+        atributo: Atributo.destreza,
+        somentetreinada: true,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Pontaria",
+        descricao: "Você usa Pontaria para fazer ataques à distância.",
+        atributo: Atributo.destreza,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Reflexos",
+        descricao: "Você usa esta perícia para resistir a efeitos que exigem reação rápida, como armadilhas e explosões.",
+        atributo: Atributo.destreza,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Religião",
+        descricao: "Você possui conhecimento sobre os deuses e as religiões de Arton.",
+        atributo: Atributo.sabedoria,
+        somentetreinada: true,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Sobrevivência",
+        descricao: "Você está em casa nos ermos.",
+        atributo: Atributo.sabedoria,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
+    {
+        nome: "Vontade",
+        descricao: "Você usa esta perícia para resistir a efeitos que exigem determinação, como intimidação e encantamentos.",
+        atributo: Atributo.sabedoria,
+        somentetreinada: false,
+        penalidadearmadura: false
+    },
 ]
+
 
 export enum TabelaPericiasEnum {
     Acrobacia = "Acrobacia",
@@ -45,5 +214,29 @@ export enum TabelaPericiasEnum {
     Atletismo = "Atletismo",
     Atuação = "Atuação",
     Cavalgar = "Cavalgar",
+    Conhecimento = "Conhecimento",
+    Cura = "Cura",
+    Diplomacia = "Diplomacia",
+    Enganação = "Enganação",
+    Fortitude = "Fortitude",
+    Furtividade = "Furtividade",
+    Guerra = "Guerra",
+    Iniciativa = "Iniciativa",
+    Intimidação = "Intimidação",
+    Intuição = "Intuição",
+    Investigação = "Investigação",
+    Jogatina = "Jogatina",
+    Ladinagem = "Ladinagem",
+    Luta = "Luta",
+    Misticismo = "Misticismo",
+    Nobreza = "Nobreza",
+    Ofício = "Ofício",
+    Percepção = "Percepção",
+    Pilotagem = "Pilotagem",
+    Pontaria = "Pontaria",
+    Reflexos = "Reflexos",
+    Religião = "Religião",
+    Sobrevivência = "Sobrevivência",
+    Vontade = "Vontade"
 }
   
