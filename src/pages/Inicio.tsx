570b893fc64860f9db7e64c5d0d9b37b2ee995ff
Pedro Bilhalva Oliveira <pedrokabilhalva@gmail.com>
2023-10-22 15:21:41 -0300

    mobile on inicio

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -8,19 +8,19 @@ const Inicio: React.FC = () => {
     <>
       <body className="bg-bgT20 bg-cover bg-bottom min-h-screen w-full">
       <Navbar titulo={"Inicio"} />
-        <article className="bg-gray-50 bg-opacity-30 w-[75%] mx-auto py-8 px-4 rounded-lg border-gray-500 shadow-lg mt-5">
+        <article className="bg-gray-50 bg-opacity-30 w-3/4 mx-auto py-8 px-4 rounded-lg border-gray-500 shadow-lg mt-5">
           <h1 className="text-3xl text-center text-red-800 drop-shadow-[_2px_2px_rgba(0,0,0,0.25)]">
             T20 CHARACTER CREATOR
           </h1>
-          <article className="flex-col justify-center mt-5 gap-5 transition-all ease-in-out desktop:flex-row">
+          <article className="flex-col flex justify-center mt-5 gap-5 transition-all ease-in-out desktop:flex-row">
             <Link to="/criar">
               <button
-                className="bg-gray-50 bg-opacity-50 p-5 text-red-950 font-bold rounded-lg hover:bg-opacity-70 transition-all ease-in-out flex gap-1"
+                className="mx-auto desktop:mx-0 bg-gray-50 bg-opacity-50 p-5 text-red-950 font-bold rounded-lg hover:bg-opacity-70 transition-all ease-in-out flex gap-1"
               >
                 <Plus /> Novo Personagem
               </button>
             </Link>
-            <button className="bg-gray-50 opacity-20 bg-opacity-50 p-5 text-red-950 font-bold rounded-lg hover:bg-opacity-70 transition-all ease-in-out flex gap-1">
+            <button className="bg-gray-50 opacity-20 bg-opacity-50 p-5 text-red-950 font-bold rounded-lg hover:bg-opacity-70 transition-all ease-in-out flex gap-1 mx-auto desktop:mx-0">
               <UserPlus />
               Meus Personagens
             </button>
