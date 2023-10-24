ea35e76e484a0fbb07b182798cc81e78ea83b4a5
Senac-Bil <pedrokabilhalva@gmail.com>
2023-10-24 10:36:55 -0300

    +classes template

---
+++ src/classes/Classes/Nobre.ts
@@ -0,0 +1,49 @@
+import { Poder, TipoPoder } from "../Construtores/Poder";
+import { Classe } from "./Classe";
+import { TabelaPericiasEnum } from "../Tabelas/Pericias";
+
+export const poderesNobre: Poder[] = [
+  {
+    nome: "Arcano de Batalha.",
+    descricao:
+      "Quando lança uma magia, você soma seu atributo-chave na rolagem de dano.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Aumento de Atributo.",
+    descricao:
+      "Você recebe +1 em um atributo. Você pode escolher este poder várias vezes, mas apenas uma vez por patamar para um mesmo atributo.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+];
+
+export const nobre: Classe = {
+  nome: "Nobre",
+  descricao:
+    "Um conjurador de magias arcanas, por meio de estudo, um foco ou dom natural.",
+  vidapnivel: 2,
+  vidainicial: 8,
+  manapnivel: 6,
+  pericias: [TabelaPericiasEnum.Misticismo, TabelaPericiasEnum.Vontade],
+  periciasescolha: [
+    TabelaPericiasEnum.Conhecimento,
+    TabelaPericiasEnum.Diplomacia,
+    TabelaPericiasEnum.Enganação,
+    TabelaPericiasEnum.Guerra,
+    TabelaPericiasEnum.Iniciativa,
+    TabelaPericiasEnum.Intimidação,
+    TabelaPericiasEnum.Intuição,
+    TabelaPericiasEnum.Investigação,
+    TabelaPericiasEnum.Nobreza,
+    TabelaPericiasEnum.Ofício,
+    TabelaPericiasEnum.Percepção,
+  ],
+  periciasescolhanum: 2,
+  proficiencias: [],
+  habilidades: ["caminho do arcanista", "alta arcana"],
+  poderesunicos: poderesNobre,
+  conjurador: true,
+  imagem: "./img/templates/Class.png",
+};
--- src/classes/Tabelas/Classes.ts
+++ src/classes/Tabelas/Classes.ts
@@ -1,7 +1,33 @@
 import { Classe } from "../Classes/Classe";
 import { arcanista } from "../Classes/Arcanista";
+import { barbaro } from "../Classes/Barbaro";
+import { bardo } from "../Classes/Bardo";
+import { bucaneiro } from "../Classes/Bucaneiro";
+import { cacador } from "../Classes/Cacador";
+import { cavaleiro } from "../Classes/Cavaleiro";
+import { clerigo } from "../Classes/Clerigo";
+import { druida } from "../Classes/Druida";
+import { guerreiro } from "../Classes/Guerreiro";
+import { inventor } from "../Classes/Inventor";
+import { ladino } from "../Classes/Ladino";
+import { lutador } from "../Classes/Lutador";
+import { nobre } from "../Classes/Nobre"; 
+import { paladino } from "../Classes/Paladino";
 
 export const TabelaClasses: Classe[] = [
     arcanista,
+    barbaro,
+    bardo,
+    bucaneiro,
+    cacador,
+    cavaleiro,
+    clerigo,
+    druida,
+    guerreiro,
+    inventor,
+    ladino,
+    lutador,
+    nobre,
+    paladino,
 ];
  
