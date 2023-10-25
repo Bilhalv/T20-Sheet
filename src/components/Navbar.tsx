18844ef457423e8f2877a962ae617b6326654321
Senac-Bil <pedrokabilhalva@gmail.com>
2023-10-25 08:55:40 -0300

    resolvida imagem

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar({ titulo }: Props) {
     <>
       <nav className="z-10 flex sticky top-0 justify-between bg-[#10000d] p-5 text-white shadow-lg">
         <Link to="/">
-        <img className="h-8" src="https://i.ibb.co/k4FHRNK/image-1.png" />
+        <img className="h-8" src="./img/bannerT20.png" />
         </Link>
         <Link to="/">
           <button className="text-lg bg-[#09000d] px-2 rounded-lg transition-all ease-in-out hover:bg-[#05000d]">
