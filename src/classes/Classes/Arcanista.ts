163bb5b13a2086f9289c17e0fc2e5adb2bb8fcd1
Diego Nachtigall <ditsguts@gmail.com>
2023-10-26 22:27:56 -0300

    + alguns poderes de arcanista

--- src/classes/Classes/Arcanista.ts
+++ src/classes/Classes/Arcanista.ts
@@ -20,10 +20,63 @@ export const poderesArcanista: Poder[] = [
   {
     nome: "Caldeirão do Bruxo.",
     descricao:
-      "Você pode criar poções, como se tivesse o poder geral Preparar Poção. Se tiver ambos, pode criar poções de até 5o círculo. Pré-requisitos: Bruxo, treinado em Ofício (alquimista).",
+    "Você pode criar poções, como se tivesse o poder geral Preparar Poção. Se tiver ambos, pode criar poções de até 5o círculo. Pré-requisitos: Bruxo, treinado em Ofício (alquimista).",
     tipo: TipoPoder.classe,
     requisitos: [RequisitoPoder.tipo_arcanista, RequisitoPoder.pericia],
+    // Bruxo, oficio(alquimista)
   },
+  {
+    nome: "Conhecimento Mágico.",
+    descricao:
+      "Você aprende duas ma- gias de qualquer círculo que possa lançar. Você pode escolher este poder quantas vezes quiser.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Contramágica Aprimorada.",
+    descricao:
+      "Uma vez por rodada, você pode fazer uma contramágica como uma reação (veja a página 173).",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.magia],
+    // Dissipar magia
+  },
+  {
+    nome: "Envolto em Mistério.",
+    descricao:
+      "Sua aparência e postura assombrosas o permitem manipular e assustar pessoas ignorantes ou supersticiosas. O mestre define o que exatamente você pode fazer e quem se encaixa nessa descrição. Como regra geral, você recebe +5 em Enganação e Intimidação contra pessoas não treinadas em Conhecimento ou Misticismo.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Escriba Arcano.",
+    descricao:
+      "Você pode aprender magias copiando os textos de pergaminhos e grimórios de outros magos. Aprender uma magia dessa forma exige um dia de trabalho e T$ 250 em matérias-primas por PM necessário para lançar a magia. Assim, aprender uma magia de 3o círculo (6 PM) exige 6 dias de trabalho e o gasto de T$ 1.500.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.tipo_arcanista, RequisitoPoder.pericia],
+    // Mago, treinado em Ofício (escriba).
+  },
+  {
+    nome: "Especialista em Escola.",
+    descricao:
+      "Especialista em Escola. Escolha uma escola de magia. A CD para resistir a suas magias dessa escola aumenta em +2.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.tipo_arcanista],
+    // Bruxo ou Mago.
+  },
+  {
+    nome: "Familiar.",
+    descricao:
+      "Você possui um animal de estimação mágico. Veja o quadro para detalhes.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  // {
+  //   nome: "",
+  //   descricao:
+  //     "",
+  //   tipo: TipoPoder.classe,
+  //   requisitos: [],
+  // },
 ];
 
 export const arcanista: Classe = {
