74760b000b4784c9f7a6da2efc3c0c5185a31186
Diego Nachtigall <ditsguts@gmail.com>
2023-10-30 23:20:46 -0300

    + bucaneiro, caçador, cavaleiro

--- src/classes/Classes/Bucaneiro.ts
+++ src/classes/Classes/Bucaneiro.ts
@@ -1,49 +1,157 @@
-import { Poder, TipoPoder } from "../Construtores/Poder";
+import { Poder, RequisitoPoder, TipoPoder } from "../Construtores/Poder";
 import { Classe } from "./Classe";
 import { TabelaPericiasEnum } from "../Tabelas/Pericias";
 
 export const poderesBucaneiro: Poder[] = [
   {
-    nome: "Arcano de Batalha.",
-    descricao:
-      "Quando lança uma magia, você soma seu atributo-chave na rolagem de dano.",
+    nome: "Abusar dos Fracos",
+    descricao: "Quando ataca uma criatura sob efeito de uma condição de medo, seu dano aumenta em um passo.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.poder],
+  },
+  {
+    nome: "Aparar",
+    descricao: "Uma vez por rodada, quando é atingido por um ataque, você pode gastar 1 PM para fazer um teste de ataque com bônus igual ao seu nível (além do normal). Se o resultado do seu teste for maior que o do oponente, você evita o ataque. Você só pode usar este poder se estiver usando uma arma corpo a corpo leve ou ágil.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.poder],
+  },
+  {
+    nome: "Apostador",
+    descricao: "Você pode gastar um dia para encontrar e participar de uma mesa de wyrt ou outro jogo de azar. Escolha um valor e faça um teste de Jogatina contra a CD correspondente: T$ 100 (CD 15), T$ 200 (CD 20), T$ 400 (CD 25), T$ 800 (CD 30), T$ 1.600 (CD 35) e assim por diante. Se passar, você ganha o valor escolhido (ou um item ou favor equivalente, a critério do mestre). Se falhar, perde esse mesmo o valor. A critério do mestre, o lugar onde você está pode limitar ou impossibilitar o uso deste poder.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.pericia],
+  },
+  
+  {
+    nome: "Ataque Acrobático",
+    descricao: "Quando se aproxima de um inimigo com um salto ou pirueta (em termos de jogo, usando Atletismo ou Acrobacia para se mover) e o ataca no mesmo turno, você recebe +2 nesse teste de ataque e na rolagem de dano.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  
+  {
+    nome: "Aumento de Atributo",
+    descricao: "Você recebe +1 em um atributo. Você pode escolher este poder várias vezes, mas apenas uma vez por patamar para um mesmo atributo.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  
+  {
+    nome: "Aventureiro Ávido",
+    descricao: "Uma vez por rodada, você pode gastar 5 PM para realizar uma ação padrão ou de movimento adicional. Se possuir o poder Surto Heroico, em vez disso seu custo diminui em -2 PM.",
     tipo: TipoPoder.classe,
     requisitos: [],
   },
   {
-    nome: "Aumento de Atributo.",
-    descricao:
-      "Você recebe +1 em um atributo. Você pode escolher este poder várias vezes, mas apenas uma vez por patamar para um mesmo atributo.",
+    nome: "Bravata Audaz",
+    descricao: "Você jura fazer uma façanha específica, como roubar o tesouro de Sckhar ou ganhar um beijo do príncipe e da princesa até o fim do baile. Se cumprir a bravata, seus PM aumentam em +2 por nível de bucaneiro até o fim da aventura.",
     tipo: TipoPoder.classe,
     requisitos: [],
+  },
+  {
+    nome: "Bravata Imprudente",
+    descricao: "Na primeira rodada de um combate, você pode jurar derrotar seus inimigos com uma restrição a sua escolha, como lutar com uma mão nas costas, de guarda aberta (em termos de jogo, desprevenido), de olhos vendados (cego) etc. Uma restrição só é válida se prejudicá-lo (por exemplo, lutar com uma mão nas costas só vale como restrição se você luta com duas armas). O mestre tem a palavra final sobre a validade de uma restrição. Você sofre a penalidade durante todo o combate, mas, se vencer, recebe +2 nos testes de ataque e na margem de ameaça até o fim da aventura.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "En Garde",
+    descricao: "Você pode gastar uma ação de movimento e 1 PM para assumir postura de luta. Até o fim da cena, se estiver usando uma arma corpo a corpo leve ou ágil, você recebe +2 na margem de ameaça com essas armas e +2 na Defesa.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.poder],
+  },
+  {
+    nome: "Esgrimista",
+    descricao: "Quando usa uma arma corpo a corpo leve ou ágil, você soma sua Inteligência nas rolagens de dano (limitado pelo seu nível).",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.atributo],
+  },
+  {
+    nome: "Flagelo dos Mares",
+    descricao: "Você aprende e pode lançar Amedrontar (atributo-chave Carisma). Esta não é uma habilidade mágica e provém de sua capacidade de incutir medo em seus inimigos.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.pericia],
+  },
+  {
+    nome: "Folião",
+    descricao: "Você sabe fazer amizades durante festas, de noitadas em tavernas a bailes na corte. Nesses locais, você recebe +2 em testes de perícias de Carisma e a atitude de todas as pessoas em relação a você melhora em uma categoria.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.atributo],
+  },
+  {
+    nome: "Grudar o Cano",
+    descricao: "Quando faz um ataque à distância com uma arma de fogo contra um oponente adjacente, você não sofre a penalidade de -5 no teste de ataque e aumenta seu dano em um passo.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.pericia, RequisitoPoder.poder],
+  },
+  {
+    nome: "Pernas do Mar",
+    descricao: "Você recebe +2 em Acrobacia e Atletismo. Além disso, quando está se equilibrando ou escalando, você não fica desprevenido e seu deslocamento não é reduzido à metade.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Pistoleiro",
+    descricao: "Você recebe proficiência com armas de fogo e +2 nas rolagens de dano com essas armas.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Presença Paralisante",
+    descricao: "Você soma seu Carisma em Iniciativa e, se for o primeiro na iniciativa, ganha uma ação padrão extra na primeira rodada.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.atributo,RequisitoPoder.nivel],
   },
 ];
 
 export const bucaneiro: Classe = {
   nome: "Bucaneiro",
   descricao:
-    "Um conjurador de magias arcanas, por meio de estudo, um foco ou dom natural.",
-  vidapnivel: 2,
-  vidainicial: 8,
-  manapnivel: 6,
-  pericias: [TabelaPericiasEnum.Misticismo, TabelaPericiasEnum.Vontade],
+    "Bucaneiros são aventureiros que singram as águas deste mundo, sempre metidos em missões, batalhas, buscas, patrulhas ou a simples luta pela sobrevivência.",
+  vidapnivel: 4,
+  vidainicial: 16,
+  manapnivel: 3,
+  pericias: [TabelaPericiasEnum.Luta, TabelaPericiasEnum.Pontaria, TabelaPericiasEnum.Reflexos],
   periciasescolha: [
-    TabelaPericiasEnum.Conhecimento,
-    TabelaPericiasEnum.Diplomacia,
+    TabelaPericiasEnum.Acrobacia,
+    TabelaPericiasEnum.Atletismo,
+    TabelaPericiasEnum.Atuação,
     TabelaPericiasEnum.Enganação,
-    TabelaPericiasEnum.Guerra,
+    TabelaPericiasEnum.Fortitude,
+    TabelaPericiasEnum.Furtividade,
     TabelaPericiasEnum.Iniciativa,
     TabelaPericiasEnum.Intimidação,
-    TabelaPericiasEnum.Intuição,
-    TabelaPericiasEnum.Investigação,
-    TabelaPericiasEnum.Nobreza,
+    TabelaPericiasEnum.Jogatina,
     TabelaPericiasEnum.Ofício,
     TabelaPericiasEnum.Percepção,
+    TabelaPericiasEnum.Pilotagem,
+    TabelaPericiasEnum.Pontaria
   ],
-  periciasescolhanum: 2,
-  proficiencias: [],
-  habilidades: [],
+  periciasescolhanum: 4,
+  proficiencias: ["Armas marciais"],
+  habilidades: [
+    {
+      nome: "Audácia",
+      descricao: "Quando faz um teste de perícia, você pode gastar 2 PM para somar seu Carisma no teste. Você não pode usar esta habilidade em testes de ataque."
+    },
+    {
+      nome: "Insolência",
+      descricao: "Você soma seu Carisma na Defesa, limitado pelo seu nível. Esta habilidade exige liberdade de movimentos; você não pode usá-la se estiver de armadura pesada ou na condição imóvel."
+    },
+    {
+      nome: "Evasão",
+      descricao: "A partir do 2o nível, quando sofre um efeito que permite um teste de Reflexos para reduzir o dano à metade, você não sofre dano algum se passar. Você ainda sofre dano normal se falhar no teste de Reflexos. Esta habilidade exige liberdade de movimentos; você não pode usá-la se estiver de armadura pesada ou na condição imóvel."
+    },
+    {
+      nome: "Esquiva Sagaz",
+      descricao: "No 3o nível, você recebe +1 na Defesa e em Reflexos. Esse bônus aumenta em +1 a cada quatro níveis. Esta habilidade exige liberdade de movimentos; você não pode usá-la se estiver de armadura pesada ou na condição imóvel."
+    },
+    {
+      nome: "Panache",
+      descricao: " A partir do 5o nível, sempre que faz um acerto crítico em combate ou reduz um inimigo a 0 PV, você recupera 1 PM."
+    },
+  ],
   poderesunicos: poderesBucaneiro,
-  conjurador: true,
+  conjurador: false,
   imagem: "./img/classes/bucaneiro.png",
 };
--- src/classes/Classes/Cacador.ts
+++ src/classes/Classes/Cacador.ts
@@ -1,49 +1,169 @@
-import { Poder, TipoPoder } from "../Construtores/Poder";
+import { Poder, RequisitoPoder, TipoPoder } from "../Construtores/Poder";
 import { Classe } from "./Classe";
 import { TabelaPericiasEnum } from "../Tabelas/Pericias";
 
 export const poderesCacador: Poder[] = [
   {
-    nome: "Arcano de Batalha.",
-    descricao:
-      "Quando lança uma magia, você soma seu atributo-chave na rolagem de dano.",
+    nome: "Ambidestria",
+    descricao: "Se estiver empunhando duas armas (e pelo menos uma delas for leve) e fizer a ação agredir, você pode fazer dois ataques, um com cada arma. Se fizer isso, sofre –2 em todos os testes de ataque até o seu próximo turno.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.atributo],
+  },
+  {
+    nome: "Armadilha: Arataca",
+    descricao: "A vítima sofre 2d6 pontos de dano de perfuração e fica agarrada. Uma criatura agarrada pode escapar com uma ação padrão e um teste de Força ou Acrobacia (CD Sab).",
     tipo: TipoPoder.classe,
     requisitos: [],
   },
   {
-    nome: "Aumento de Atributo.",
-    descricao:
-      "Você recebe +1 em um atributo. Você pode escolher este poder várias vezes, mas apenas uma vez por patamar para um mesmo atributo.",
+    nome: "Armadilha: Espinhos",
+    descricao: "A vítima sofre 6d6 pontos de dano de perfuração. Um teste de Reflexos (CD Sab) reduz o dano à metade.",
     tipo: TipoPoder.classe,
     requisitos: [],
   },
+  {
+    nome: "Armadilha: Laço",
+    descricao: "A vítima deve fazer um teste de Reflexos (CD Sab). Se passar, fica caída. Se falhar, fica agarrada. Uma criatura agarrada pode se soltar com uma ação padrão e um teste de Força ou Acrobacia (CD Sab).",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Armadilha: Rede",
+    descricao: "Todas as criaturas na área ficam enredadas e não podem sair da área. Uma vítima pode se libertar com uma ação padrão e um teste de Força ou Acrobacia (CD 25). Além disso, a área ocupada pela rede é considerada terreno difícil. Nesta armadilha você escolhe quantas criaturas precisam estar na área para ativá-la.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Armadilheiro",
+    descricao: "A CD para encontrar e resistir às suas armadilhas aumenta em +5 e você soma sua Sabedoria ao dano delas.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.poder, RequisitoPoder.nivel],
+  },
+  {
+    nome: "Arqueiro",
+    descricao: "Se estiver usando uma arma de ataque à distância, você soma sua Sabedoria nas rolagens de dano (limitado pelo seu nível).",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.atributo],
+  },
+  {
+    nome: "Aumento de Atributo",
+    descricao: "Você recebe +1 em um atributo. Você pode escolher este poder várias vezes, mas apenas uma vez por patamar para um mesmo atributo.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Camuflagem",
+    descricao: "Você pode gastar 2 PM para se esconder mesmo sem camuflagem ou cobertura disponível. Pré-requisito: 6o nível de caçador.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Companheiro Animal",
+    descricao: "Você recebe um companheiro animal. Veja o quadro na página 62.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.atributo,RequisitoPoder.pericia],
+  },
+  {
+    nome: "Elo com a Natureza",
+    descricao: "Você soma sua Sabedoria em seu total de pontos de mana e aprende e pode lançar Caminhos da Natureza (atributo-chave Sabedoria).",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.atributo,RequisitoPoder.nivel],
+  },
+  {
+    nome: "Emboscar",
+    descricao: "Você pode gastar 2 PM para realizar uma ação padrão adicional em seu turno. Você só pode usar este poder na primeira rodada de um combate.",
+    tipo: TipoPoder.classe,
+    requisitos: [RequisitoPoder.pericia],
+  },
+  {
+    nome: "Empatia Selvagem",
+    descricao: "Você pode se comunicar com animais por meio de linguagem corporal e vocalizações. Você pode usar Adestramento com animais para mudar atitude e persuasão (veja Diplomacia, na página 118).",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Espreitar",
+    descricao: "Quando usa a habilidade Marca da Presa, você recebe um bônus de +1 em testes de perícia contra a criatura marcada. Esse bônus aumenta em +1 para cada PM adicional gasto na habilidade e também dobra com a habilidade Inimigo.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Ervas Curativas",
+    descricao: "Você pode gastar uma ação completa e uma quantidade de PM a sua escolha (limitado por sua Sabedoria) para aplicar ervas que curam ou desintoxicam em você ou num aliado adjacente. Para cada PM que gastar, cura 2d6 PV ou remove uma condição envenenado afetando o alvo.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Ímpeto",
+    descricao: "Você pode gastar 1 PM para aumentar seu deslocamento em +6m por uma rodada.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Inimigo de (Criatura)",
+    descricao: "Escolha um tipo de criatura entre animal, construto, espírito, monstro ou morto-vivo, ou duas raças humanoides (por exemplo, orcs e gnolls, ou elfos e qareen). Quando você usa a habilidade Marca da Presa contra uma criatura do tipo ou da raça escolhida, dobra os dados de bônus no dano. O nome desta habilidade varia de acordo com o tipo de criatura escolhida (Inimigo de Monstros, Inimigo de Mortos-Vivos etc.). Você pode escolher este poder outras vezes para inimigos diferentes.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Olho do Falcão",
+    descricao: "Você pode usar a habilidade Marca da Presa em criaturas em alcance longo.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  },
+  {
+    nome: "Ponto Fraco",
+    descricao: "Quando usa a habilidade Marca da Presa, seus ataques contra a criatura marcada recebem +2 na margem de ameaça. Esse bônus dobra com a habilidade Inimigo.",
+    tipo: TipoPoder.classe,
+    requisitos: [],
+  }
+  
 ];
 
 export const cacador: Classe = {
   nome: "Caçador",
   descricao:
-    "Um conjurador de magias arcanas, por meio de estudo, um foco ou dom natural.",
-  vidapnivel: 2,
-  vidainicial: 8,
-  manapnivel: 6,
-  pericias: [TabelaPericiasEnum.Misticismo, TabelaPericiasEnum.Vontade],
-  periciasescolha: [
-    TabelaPericiasEnum.Conhecimento,
-    TabelaPericiasEnum.Diplomacia,
-    TabelaPericiasEnum.Enganação,
-    TabelaPericiasEnum.Guerra,
+    "O caçador é um especialista em sobrevivência nos terrenos mais selvagens e inóspitos,capaz de obter alimento e achar abrigo em qualquer lugar.",
+  vidapnivel: 4,
+  vidainicial: 16,
+  manapnivel: 4,
+  pericias: [TabelaPericiasEnum.Luta, TabelaPericiasEnum.Pontaria, TabelaPericiasEnum.Sobrevivência],
+    periciasescolha: [
+    TabelaPericiasEnum.Adestramento,
+    TabelaPericiasEnum.Atletismo,
+    TabelaPericiasEnum.Cavalgar,
+    TabelaPericiasEnum.Cura,
+    TabelaPericiasEnum.Fortitude,
+    TabelaPericiasEnum.Furtividade,
     TabelaPericiasEnum.Iniciativa,
-    TabelaPericiasEnum.Intimidação,
-    TabelaPericiasEnum.Intuição,
     TabelaPericiasEnum.Investigação,
-    TabelaPericiasEnum.Nobreza,
+    TabelaPericiasEnum.Luta,
     TabelaPericiasEnum.Ofício,
     TabelaPericiasEnum.Percepção,
+    TabelaPericiasEnum.Pontaria,
+    TabelaPericiasEnum.Reflexos,
   ],
-  periciasescolhanum: 2,
-  proficiencias: [],
-  habilidades: [],
+  periciasescolhanum: 6,
+  proficiencias: ['Armas marciais', 'escudos'],
+  habilidades: [
+    {
+      nome: "Marca da Presa",
+      descricao: "Você pode gastar uma ação de movimento e 1 PM para analisar uma criatura em alcance curto. Até o fim da cena, você recebe +1d4 nas rolagens de dano contra essa criatura. A cada quatro níveis, você pode gastar +1 PM para aumentar o bônus de dano (veja a tabela da classe)."
+    },
+    {
+      nome: "Rastreador",
+      descricao: "Você recebe +2 em Sobrevivência. Além disso, pode se mover com seu deslocamento normal enquanto rastreia sem sofrer penalidades no teste de Sobrevivência."
+    },
+    {
+      nome: "Explorador",
+      descricao: "No 3o nível, escolha um tipo de terreno entre aquático, ártico, colina, deserto, floresta, montanha, pântano, planície, subterrâneo ou urbano. A partir do 11o nível, você também pode escolher área de Tormenta. Quando estiver no tipo de terreno escolhido, você soma sua Sabedoria (mínimo +1) na Defesa e nos testes de Acrobacia, Atletismo, Furtividade, Percepção e Sobrevivência. A cada quatro níveis, escolha outro tipo de terreno para receber o bônus ou aumente o bônus em um tipo de terreno já escolhido em +2."
+    },
+    {
+      nome: "Caminho do Explorador",
+      descricao: "No 5o nível, você pode atravessar terrenos difíceis sem sofrer redução em seu deslocamento e a CD para rastrear você aumenta em +10. Esta habilidade só funciona em terrenos nos quais você tenha a habilidade Explorador."
+    },
+  ],
   poderesunicos: poderesCacador,
-  conjurador: true,
+  conjurador: false,
   imagem: "./img/classes/cacador.png",
 };
