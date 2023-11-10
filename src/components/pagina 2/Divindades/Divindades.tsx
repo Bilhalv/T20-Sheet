55693894720d666e47bf1fd79283e7768bd8e82d
Senac-Bil <pedrokabilhalva@gmail.com>
2023-11-10 12:04:43 -0300

    Refactor divindade selection and display

--- src/components/pagina 2/Divindades/Divindades.tsx
+++ src/components/pagina 2/Divindades/Divindades.tsx
@@ -13,13 +13,12 @@ export default function Divindades({ setPagina, next }: DivindadesProps) {
     const mapeado = tabelaDivindades.map((divindade) => {
       if (divindade.nome === nome) {
         return divindade;
+      } else {
+        return null;
       }
     });
-    return mapeado;
+    return mapeado.filter((item) => item !== null);
   };
-  const [placeholder, setPlaceholder] = useState(
-    localStorage.getItem("divindade") ?? tabelaDivindades[0].nome
-  );
   if (localStorage.getItem("divindade") === null) {
     localStorage.setItem("divindade", tabelaDivindades[0].nome);
   }
@@ -30,10 +29,12 @@ export default function Divindades({ setPagina, next }: DivindadesProps) {
   const handleSelect = (e: any) => {
     setSelected(e.target.value);
     localStorage.setItem("divindade", e.target.value);
-    setPlaceholder(e.target.value);
   };
 
-  const onChange = () => {};
+  const onChange = (e: any) => {
+    setSelected(e.target.value);
+    console.log(mapear(selected)[0]?.crencas);
+  };
   return (
     <>
       <h1 className="text-center text-lg font-bold mb-3">
@@ -53,7 +54,7 @@ export default function Divindades({ setPagina, next }: DivindadesProps) {
             <h1 className="text-lg font-bold text-center desktop:block hidden">
               {selected}
             </h1>
-            <img src={mapear(selected)[0]?.imagens[0]} className="mx-auto" />
+            <img src={mapear(selected)[0]?.imagens[0]} className="mx-auto transition-all ease-in-out" />
             <p className="text-center mt-3 font-serif ">
               {mapear(selected)[0]?.crencas}
             </p>
@@ -77,23 +78,20 @@ export default function Divindades({ setPagina, next }: DivindadesProps) {
           <div className="grid gap-5 grid-cols-4 place-items-center">
             {tabelaDivindades.map((divindade) => {
               return (
-                <div
+                <button
                   key={divindade.nome}
                   onClick={() => {
-                    setSelected(divindade.nome);
-                    setPlaceholder(divindade.nome);
+                    onChange({ target: { value: divindade.nome } });
                   }}
                   className={`${
                     selected === divindade.nome
-                      ? "bg-gray-300"
+                      ? "bg-red-950 text-red-200  "
                       : "bg-gray-200 hover:bg-gray-300"
-                  } p-2 rounded-lg shadow-lx my-2 cursor-pointer w-full hover:shadow-[0px_5px_4px_0px_rgba(0,0,0,0.25)] transition-all ease-in-out hover:scale-105`}
+                  } p-2 rounded-lg shadow-lx my-2 cursor-pointer w-full hover:shadow-[0px_5px_4px_0px_rgba(0,0,0,0.25)] transition-all ease-in-out hover:scale-105 bg-opacity-90`}
                 >
-                  <img src={divindade.imagens[1]} className="mx-auto w-12" />
-                  <h1 className="text-center">
-                    {divindade.nome}
-                  </h1>
-                </div>
+                  <img src={divindade.imagens[1]} className="mx-auto w-1/2" />
+                  <h1 className="text-center">{divindade.nome}</h1>
+                </button>
               );
             })}
           </div>
