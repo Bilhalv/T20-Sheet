124143a57807e16e7d92ae8f6d96e8764445f48e
Pedro Bilhalva Oliveira <pedrokabilhalva@gmail.com>
2023-11-18 18:32:48 -0300

    feat: construtor de magias

---
+++ src/classes/Construtores/Magia.ts
@@ -0,0 +1,39 @@
+import { infos } from "./Infos";
+
+export enum enumExecucao {
+    padrao = "Padrão",
+    completa = "Completa",
+    movimento = "Movimento",
+    reacao = "Reação",
+    livre = "Livre",
+    instantanea = "Instantanea",
+    cena = "Cena",
+    nenhuma = "Nenhuma"
+}
+
+export class aprimoramentos {
+    pm_a_mais: number = 0;
+    descricao: string = "";
+}
+
+export enum enumEscolas{
+    abjuracao = "Abjuração",
+    adivinhacao = "Adivinhação",
+    convocacao = "Convocação",
+    encantamento = "Encantamento",
+    evocacao = "Evocação",
+    ilusao = "Ilusão",
+    necromancia = "Necromancia",
+    transmutacao = "Transmutação"
+}
+
+export class Magia extends infos{
+    circulo: number = 0;
+    execucao: enumExecucao = enumExecucao.padrao;
+    alcance: string = "";
+    alvo: string = "";
+    duracao: string = "";
+    resistencia: string = "";
+    escola: enumEscolas = enumEscolas.abjuracao;
+    aprimoramentos: aprimoramentos[] = [];
+}
