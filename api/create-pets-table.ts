a532c993222445222f0fc573f8643afd90889327
Pedro Bilhalva Oliveira <pedrokabilhalva@gmail.com>
2023-11-21 20:39:18 -0300

    Delete add-pets.ts and update create-pets-table.ts

--- api/create-pets-table.ts
+++ api/create-pets-table.ts
@@ -5,11 +5,5 @@ export default async function handler(
   request: VercelRequest,
   response: VercelResponse,
 ) {
-  try {
-    const result =
-      await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
-    return response.status(200).json({ result });
-  } catch (error) {
-    return response.status(500).json({ error });
-  }
+  return response.status(200).json({ name: 'John Doe' });
 }
