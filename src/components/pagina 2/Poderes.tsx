779bac816342c3a4f5e2267750544f76862597d5
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2023-11-22 17:21:39 +0000

    style: corrigido estilo de poderes mobile

--- src/components/pagina 2/Poderes.tsx
+++ src/components/pagina 2/Poderes.tsx
@@ -163,9 +163,9 @@ export default function Poderes({ setPagina, next }: PoderesProps) {
                   key={index}
                   className="flex border-b border-gray-300 items-center py-2 gap-2"
                 >
-                  <div className="w-3/5 flex justify-between items-center">
-                    <p className="w-2/3">{poder.nome}</p>
-                    <div className="w-1/3">
+                  <div className="w-3/5 flex justify-between items-center gap-1">
+                    <p className="desktop:w-2/3">{poder.nome}</p>
+                    <div className="desktop:w-1/3 w-1/2">
                       <Popover>
                         <PopoverTrigger>
                           <button className="rounded-lg bg-red-300 text-sm py-3 px-6 transition-all hover:transform hover:scale-110 hover:bg-red-500">
