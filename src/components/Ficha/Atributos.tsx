8382f3c04b04389c98f526672fc8c4de45d9471d
Diego Nachtigall <ditsguts@gmail.com>
2023-11-29 23:44:50 -0300

    style: Update Ficha style

--- src/components/Ficha/Atributos.tsx
+++ src/components/Ficha/Atributos.tsx
@@ -5,7 +5,7 @@ interface Props {
 export default function Atributos({ personagem }: Props) {
   return (
     <>
-      <div className="flex justify-evenly w-full gap-4">
+      <div className="flex justify-evenly w-fit gap-4">
         {Object.keys(personagem.atributos).map((atributo) => (
           <div className="bg-gray-200 rounded-xl border border-black flex flex-col p-3 w-13 h-20 z-10">
             <h1 className="text-center mt-[-15px] text-red-900 drop-shadow-2xl text-md">
@@ -17,7 +17,7 @@ export default function Atributos({ personagem }: Props) {
             </h1>
           </div>
         ))}
-        <div className="bg-red-800 absolute z-0 mt-5 h-11 w-3/5 desktop:w-[408px]" ></div>
+        <div className="bg-red-800 absolute z-0 mt-5 h-11 w-4/5 desktop:w-[408px]" ></div>
       </div>
     </>
   );
