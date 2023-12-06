00c1b5c68ec4855caa7e88595a7e46304bc35e7f
Pedro Bilhalva Oliveira <pedrokabilhalva@gmail.com>
2023-12-06 18:59:10 -0300

    shore: rolarDado crita!

--- src/components/Geral/RolarDado.ts
+++ src/components/Geral/RolarDado.ts
@@ -1,19 +1,27 @@
 interface RolarDadoProps {
-  qtd: number;
   lados: number;
+  qtd?: number;
   mod?: number;
   descarte?: number;
   rerola?: "adv" | "des";
+  ameaca?: number;
 }
 
-export function RolarDado({qtd, lados, mod, descarte, rerola}: RolarDadoProps) {
-  qtd = Math.max(qtd, 1);
+export function RolarDado({
+  qtd,
+  lados,
+  mod,
+  descarte,
+  rerola,
+  ameaca,
+}: RolarDadoProps) {
+  qtd = Math.max(qtd || 1, 1);
   lados = Math.max(lados, 1);
   descarte = descarte !== undefined && descarte > qtd ? qtd : descarte;
   let dadosNormais: number[] = [];
   let dadosDescarte: number[] = [];
   let Reroll: number[] = [];
-  let total = mod||0;
+  let total = mod || 0;
   let totalReroll = 0;
   for (let i = 0; i < qtd; i++) {
     let dado = Math.floor(Math.random() * lados) + 1;
@@ -40,5 +48,6 @@ export function RolarDado({qtd, lados, mod, descarte, rerola}: RolarDadoProps) {
     descarte: dadosDescarte,
     reroll: Reroll,
     total: total,
+    critou: dadosNormais[0] >= (ameaca || lados),
   };
 }
