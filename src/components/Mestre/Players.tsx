64171c8b9727c8a8f3fe5ec2b31b9980bdc46c14
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2023-12-30 19:06:15 +0000

    feat: atributos dos players mostrando

--- src/components/Mestre/Players.tsx
+++ src/components/Mestre/Players.tsx
@@ -138,15 +138,51 @@ export default function Players({ player, setPlayersLista }: Props) {
         </PopoverContent>
       </Popover>
       <div className="flex justify-between font-bold text-red-700 font-tormenta text-2xl">
-        <h1>
-          {player.nome}
-        </h1>
+        <h1>{player.nome}</h1>
         <h1>{player.nivel}</h1>
       </div>
-      <div className="italic text-sm w-full border-b-2 border-b-red-600 text-gray-600">
+      <div className="italic text-sm w-full text-gray-600">
         <h1>
           {player.raca} {player.classe} ({player.player})
         </h1>
+      </div>
+      <div className="border-y-2 border-y-red-600 flex justify-evenly text-xs">
+        {[
+          {
+            nome: "Força",
+            valor: player.atributos.for,
+          },
+          {
+            nome: "Destreza",
+            valor: player.atributos.des,
+          },
+          {
+            nome: "Constituição",
+            valor: player.atributos.con,
+          },
+          {
+            nome: "Inteligência",
+            valor: player.atributos.int,
+          },
+          {
+            nome: "Sabedoria",
+            valor: player.atributos.sab,
+          },
+          {
+            nome: "Carisma",
+            valor: player.atributos.car,
+          },
+        ].map((atributo) => (
+          <div className="flex gap-2 items-center">
+            <p className="text-red-600 font-bold">
+              {atributo.nome.substring(0, 3).toUpperCase()}
+            </p>
+            <p>
+              {atributo.valor > 0 && "+"}
+              {atributo.valor}
+            </p>
+          </div>
+        ))}
       </div>
       <div className="text-sm">
         <div className="flex gap-3">
@@ -168,246 +204,3 @@ export default function Players({ player, setPlayersLista }: Props) {
     </div>
   );
 }
-/*
-      <div className="text-sm">
-        <div className="flex gap-3">
-          <a
-            className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all"
-            onClick={() => rolar(npc.iniciativa, `Iniciativa`, "Pericia")}
-          >
-            <h1>
-              <b className="text-red-600">Iniciativa </b>
-              {npc.iniciativa > 0 && "+"}
-              {npc.iniciativa}
-            </h1>
-          </a>
-          <a
-            className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all"
-            onClick={() => rolar(npc.percepcao, "Percepção", "Pericia")}
-          >
-            <h1>
-              <b className="text-red-600">Percepção </b>
-              {npc.percepcao > 0 && "+"}
-              {npc.percepcao}
-            </h1>
-          </a>
-        </div>
-        <div className="flex">
-          <h1>
-            <b className="text-red-600">Defesa</b> {npc.defesa}
-          </h1>
-          <p>,&nbsp;</p>
-          <a
-            className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all"
-            onClick={() => rolar(npc.fort, "Fortitude", "Pericia")}
-          >
-            <h1>
-              <b className="text-red-600">Fort </b>
-              {npc.fort > 0 && "+"}
-              {npc.fort}
-            </h1>
-          </a>
-          <p>,&nbsp;</p>
-          <a
-            className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all"
-            onClick={() => rolar(npc.ref, "Reflexos", "Pericia")}
-          >
-            <h1>
-              <b className="text-red-600">Ref </b>
-              {npc.ref > 0 && "+"}
-              {npc.ref}
-            </h1>
-          </a>
-          <p>,&nbsp;</p>
-          <a
-            className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all"
-            onClick={() => rolar(npc.von, "Vontade", "Pericia")}
-          >
-            <h1>
-              <b className="text-red-600">Von </b>
-              {npc.von > 0 && "+"}
-              {npc.von}
-            </h1>
-          </a>
-        </div>
-        <h1>
-          <b className="text-red-600">Resistências </b>
-          {npc.resistencias?.join(", ") || "Nenhuma"}
-        </h1>
-        <h1 className="border-b-2 border-b-red-600">
-          <b className="text-red-600">Deslocamento </b>
-          {npc.deslocamento}m ({npc.deslocamento / 1.5}▢)
-        </h1>
-        {npc.corpoAcorpo && npc.corpoAcorpo?.length > 0 && (
-          <div className="flex gap-2">
-            <div className="flex flex-col">
-              <h1 className="text-red-600 font-bold">Corpo a Corpo </h1>
-              {npc.corpoAcorpo?.map((arma: ataque) => (
-                <div className="flex gap-2 ml-4">
-                  <a
-                    className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all text-red-700 font-bold"
-                    onClick={() =>
-                      ataqueRoll(
-                        arma.nome,
-                        arma.mod,
-                        arma.dano,
-                        arma.modDano,
-                        arma.crit
-                      )
-                    }
-                  >
-                    {arma.nome}{" "}
-                  </a>
-                  <p>
-                    {arma.mod > 0 && "+"}
-                    {arma.mod}
-                  </p>
-                  <p>
-                    ({arma.dano}+{arma.modDano})
-                  </p>
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-        {npc.distancia && npc.distancia?.length > 0 && (
-          <div className="flex gap-2">
-            <div className="flex flex-col">
-              <h1 className="text-red-600 font-bold">Distância </h1>
-              {npc.distancia?.map((arma: ataque) => (
-                <div className="flex gap-2 ml-4">
-                  <a className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all text-red-700 font-bold">
-                    {arma.nome}{" "}
-                  </a>
-                  <p>
-                    {arma.mod > 0 && "+"}
-                    {arma.mod}
-                  </p>
-                  <p>
-                    ({arma.dano}+{arma.modDano})
-                  </p>
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-        {npc.magias && npc.magias?.length > 0 && (
-          <div className="flex gap-2">
-            <h1 className="text-red-600 font-bold">Magias </h1>
-            <div>
-              {npc.magias?.map((magia: Magia) => (
-                <div className="flex gap-2">
-                  <a className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all text-red-700 font-bold">
-                    {magia.nome}{" "}
-                  </a>
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-        {npc.poderes && npc.poderes?.length > 0 && (
-          <div>
-            {npc.poderes?.map((poder: poder) => (
-              <div className="flex gap-2">
-                <p className="text-justify">
-                  <b className="text-red-600">
-                    {poder.nome}
-                    &nbsp;{poder.execucao && `(${poder.execucao})`}
-                  </b>
-                  &nbsp;
-                  {poder.descricao}
-                </p>
-              </div>
-            ))}
-          </div>
-        )}
-        <div className="border-y-2 border-y-red-600 flex justify-evenly">
-          {[
-            { nome: "Força", valor: npc.atributos.for },
-            { nome: "Destreza", valor: npc.atributos.des },
-            { nome: "Constituição", valor: npc.atributos.con },
-            { nome: "Inteligência", valor: npc.atributos.int },
-            { nome: "Sabedoria", valor: npc.atributos.sab },
-            { nome: "Carisma", valor: npc.atributos.car },
-          ].map((atributo) => (
-            <a
-              className="flex gap-2 items-center hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all"
-              onClick={() => rolar(atributo.valor, atributo.nome, "Atributo")}
-            >
-              <h1 className="text-red-600 font-bold">
-                {atributo.nome.substring(0, 3).toUpperCase()}
-              </h1>
-              <h1>
-                {atributo.valor > 0 && "+"}
-                {atributo.valor}
-              </h1>
-            </a>
-          ))}
-        </div>
-        <div className="flex gap-2">
-          <h1 className="text-red-600 font-bold">Perícias </h1>
-          <div className="flex flex-wrap gap-2">
-            {npc.pericias?.map((pericia) => (
-              <a
-                className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all"
-                onClick={() => rolar(pericia.mod, pericia.nome, "Pericia")}
-              >
-                <h1>
-                  {pericia.nome}
-                  {pericia.mod > 0 && "+"}
-                  {pericia.mod}
-                </h1>
-              </a>
-            ))}
-          </div>
-        </div>
-        {npc.equipamentos && npc.equipamentos?.length > 0 && (
-          <div className="flex gap-2">
-            <h1 className="text-red-600 font-bold">Equipamentos </h1>
-            <div className="flex flex-wrap gap-2">
-              {npc.equipamentos?.map((equipamento) => (
-                <a className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all">
-                  <h1>{equipamento}</h1>
-                </a>
-              ))}
-            </div>
-          </div>
-        )}
-        <div className="flex gap-2">
-          <p className="text-justify">
-            {!npc.tesouroFinal.includes("") ? (
-              <>
-                <a
-                  onClick={() => teasureReroll(npc)}
-                  className="hover:bg-red-600 hover:bg-opacity-10 hover:cursor-pointer hover:select-none transition-all text-red-600 font-bold mr-1"
-                >
-                  Tesouro
-                </a>{" "}
-                {!(
-                  npc.tesouroFinal[0] === "Nenhum" &&
-                  npc.tesouroFinal[1] === "Nenhum"
-                ) ? (
-                  <>
-                    {npc.tesouroFinal[0] !== "Nenhum" && (
-                      <>Itens: {npc.tesouroFinal[0]}</>
-                    )}
-                    {!npc.tesouroFinal.includes("Nenhum") && " | "}
-                    {npc.tesouroFinal[1] !== "Nenhum" && (
-                      <>Dinheiro: {npc.tesouroFinal[1]}</>
-                    )}
-                  </>
-                ) : (
-                  <>Nenhum</>
-                )}
-              </>
-            ) : (
-              <>
-                <b className="text-red-600">Tesouro </b>
-                {npc.tesouro}
-              </>
-            )}
-          </p>
-        </div>
-      </div>
-    </div>
-*/
