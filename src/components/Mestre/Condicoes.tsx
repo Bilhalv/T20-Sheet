17115c8e9af08da6fbe0c6a88baaf8100adb5447
Pedro Bilhalva Oliveira <128099321+Bilhalv@users.noreply.github.com>
2023-12-30 00:05:20 +0000

    style: condições em accordion

--- src/components/Mestre/Condicoes.tsx
+++ src/components/Mestre/Condicoes.tsx
@@ -9,6 +9,10 @@ import {
   DrawerFooter,
   Button,
   useToast,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
 } from "@chakra-ui/react";
 import { Pin, PinOff, Brain } from "lucide-react";
 import Condicao from "../../classes/Construtores/Condicao";
@@ -30,73 +34,80 @@ export default function Condicoes() {
           <DrawerCloseButton />
           <DrawerHeader>Condições</DrawerHeader>
           <DrawerBody>
-            {TabelaCondicoes.map((condicao: Condicao) => (
-              <div className="flex flex-col mb-2 p-2 border-red-600 rounded-xl border">
-                <div className="flex justify-between">
-                  <h1 className="text-red-600 text-lg font-bold">
-                    {condicao.nome}
-                  </h1>
-                  <IconButton
-                    icon={<Pin size={18} />}
-                    overflow={"hidden"}
-                    onClick={() => {
-                      toast({
-                        position: "top-right",
-                        duration: null,
-                        isClosable: true,
-                        render: ({ onClose }) => (
-                          <div className="bg-red-600 text-white p-4 rounded-xl w-1/2 ml-auto relative">
-                            <div className="flex justify-between">
-                              <h1 className="text-xl font-bold font-tormenta">
-                                {condicao.nome}
-                              </h1>
-                              <IconButton
-                                aria-label="Fechar"
-                                icon={<PinOff size={18} />}
-                                onClick={onClose}
-                                rounded={"full"}
-                                bgColor={"red"}
-                                color={"white"}
-                                size="xs"
-                                _hover={{
-                                  color: "red",
-                                  transform: "scale(1.1)",
-                                  zIndex: 1,
-                                  borderColor: "red",
-                                  bg: "transparent",
-                                  border: "2px solid",
-                                }}
-                              />
+            <Accordion allowToggle>
+              {TabelaCondicoes.map((condicao: Condicao) => (
+                <AccordionItem
+                  borderColor={"red"}
+                  rounded={"xl"}
+                  border={("1px solid red")}
+                  mb={2}
+                >
+                  <AccordionButton className="flex justify-between">
+                    <h1 className="text-red-600 text-lg font-bold">
+                      {condicao.nome}
+                    </h1>
+                    <IconButton
+                      icon={<Pin size={18} />}
+                      overflow={"hidden"}
+                      onClick={() => {
+                        toast({
+                          position: "top-right",
+                          duration: null,
+                          isClosable: true,
+                          render: ({ onClose }) => (
+                            <div className="bg-red-600 text-white p-4 rounded-xl w-1/2 ml-auto relative">
+                              <div className="flex justify-between">
+                                <h1 className="text-xl font-bold font-tormenta">
+                                  {condicao.nome}
+                                </h1>
+                                <IconButton
+                                  aria-label="Fechar"
+                                  icon={<PinOff size={18} />}
+                                  onClick={onClose}
+                                  rounded={"full"}
+                                  bgColor={"red"}
+                                  color={"white"}
+                                  size="xs"
+                                  _hover={{
+                                    color: "red",
+                                    transform: "scale(1.1)",
+                                    zIndex: 1,
+                                    borderColor: "red",
+                                    bg: "transparent",
+                                    border: "2px solid",
+                                  }}
+                                />
+                              </div>
+                              <p className="text-justify text-sm">
+                                &nbsp;&nbsp;{condicao.descricao}
+                                <i>&nbsp;{condicao?.tipo}</i>
+                              </p>
                             </div>
-                            <p className="text-justify text-sm">
-                              &nbsp;&nbsp;{condicao.descricao}
-                              <i>&nbsp;{condicao?.tipo}</i>
-                            </p>
-                          </div>
-                        ),
-                      });
-                    }}
-                    aria-label="Condições"
-                    rounded={"full"}
-                    bgColor={"red"}
-                    color={"white"}
-                    size="xs"
-                    _hover={{
-                      color: "red",
-                      transform: "scale(1.1)",
-                      zIndex: 1,
-                      borderColor: "red",
-                      bg: "transparent",
-                      border: "2px solid",
-                    }}
-                  />
-                </div>
-                <p className="text-justify font-serif text-sm">
-                  &nbsp;&nbsp;{condicao.descricao}
-                  <i>&nbsp;{condicao?.tipo}</i>
-                </p>
-              </div>
-            ))}
+                          ),
+                        });
+                      }}
+                      aria-label="Condições"
+                      rounded={"full"}
+                      bgColor={"red"}
+                      color={"white"}
+                      size="xs"
+                      _hover={{
+                        color: "red",
+                        transform: "scale(1.1)",
+                        zIndex: 1,
+                        borderColor: "red",
+                        bg: "transparent",
+                        border: "2px solid",
+                      }}
+                    />
+                  </AccordionButton>
+                  <AccordionPanel className="text-justify font-serif text-sm">
+                    &nbsp;&nbsp;{condicao.descricao}
+                    <i>&nbsp;{condicao?.tipo}</i>
+                  </AccordionPanel>
+                </AccordionItem>
+              ))}
+            </Accordion>
           </DrawerBody>
           <DrawerFooter>
             <Button
