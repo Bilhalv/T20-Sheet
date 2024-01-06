3c7d79066e34d421a0da92c725f52f6954afc26b
Pedro Bilhalva Oliveira <pedrokabilhalva@gmail.com>
2024-01-06 17:16:23 -0300

    feat: items null

--- src/classes/Tabelas/Mestre/Players.ts
+++ src/classes/Tabelas/Mestre/Players.ts
@@ -1,5 +1,6 @@
 import { Arma } from "../../Construtores/Arma";
 import { Armadura } from "../../Construtores/Armadura";
+import { Item } from "../../Construtores/Item";
 import {
   Magia,
   enumEscolas,
@@ -7,7 +8,7 @@ import {
   enumTipo,
 } from "../../Construtores/Magia";
 import Player from "../../Construtores/Mestre/Player";
-import { TabelasArmasSimles, tabelaArmaduras, tabelaArmas } from "../Itens";
+import { TabelasArmasSimles, tabelaArmaduras, tabelaArmas, tabelaItens } from "../Itens";
 import { TabelaMagias } from "../Magias";
 
 const MagiaNull: Magia = {
@@ -54,6 +55,14 @@ const ArmaduraNull: Armadura = {
   tipo: "Armadura não encontrada",
 };
 
+const ItemNull: Item = {
+  nome: "Item não encontrado",
+  descricao: "Item não encontrado",
+  espacos: 0,
+  preco: 0,
+  categoria: "Item não encontrado",
+};
+
 function FindMagia(nome: string): Magia {
   return TabelaMagias.find((magia) => magia.nome === nome) || MagiaNull;
 }
@@ -69,6 +78,12 @@ function FindArma(nome: string): Arma {
 function FindArmadura(nome: string): Armadura {
   return (
     tabelaArmaduras.find((armadura) => armadura.nome === nome) || ArmaduraNull
+  );
+}
+
+function FindItem(nome: string): Item {
+  return (
+    tabelaItens.find((item) => item.nome === nome) || ItemNull
   );
 }
 
@@ -141,28 +156,50 @@ export const TabelaPlayer: Player[] = [
     player: "Diggo",
     classe: "Clérigo",
     raca: "Aggelus",
-    defesa: 0,
+    defesa: 16,
     atributos: {
       for: 0,
       des: 0,
-      con: 0,
-      int: 0,
-      sab: 0,
+      con: 2,
+      int: 1,
+      sab: 5,
       car: 0,
     },
-    pvMax: 10,
-    pvAtual: 10,
-    pmMax: 10,
-    pmAtual: 10,
+    pvMax: 24,
+    pvAtual: 24,
+    pmMax: 15,
+    pmAtual: 15,
     poderes: [
       {
-        nome: "Lorem",
-        descricao: "Ipsum",
-        execucao: "Dolor",
+        nome: "Canalizar Energia Positiva",
+        descricao:
+          "Você pode gastar uma ação padrão e 1PM para liberar um aonda de energia positiva que afeta todas as criaturas em alcance curto. energia positiva cura 1d6 pontos de dano em criaturas vivas a sua escolha e causa 1d6 pontos de dano de luz em mortos-vivos. Uma criatura que sofra dano tem direito a um teste de Vontade (CD 11) para reduzi-lo à metade. Para cada 2PM extras que pode gastar, a cura ou dano aumenta em +1d6 PV.",
+        execucao: "Padrão",
+      },
+      {
+        nome: "Herança Divina",
+        descricao:
+          "Você é uma criatura do tipo espírito e recebe visão no escuro.",
+      },
+      {
+        nome: "Luz Sagrada",
+        descricao: "Você recebe +2 em Diplomacia e Intuição.",
       },
     ],
-    magias: [FindMagia("Curar Ferimentos")],
-    mochila: [FindArma("Adaga"), FindArmadura("Gibão de peles")],
+    magias: [
+      FindMagia("Curar Ferimentos"),
+      FindMagia("Luz"),
+      FindMagia("Bênção"),
+      FindMagia("Comando"),
+    ],
+    mochila: [
+      FindArma("Maça"),
+      FindArmadura("Brunea"),
+      FindArmadura("Escudo Leve"),
+      FindItem("Mochila"),
+      FindItem("Saco de dormir"),
+      FindItem("Traje de Sacerdote"),
+    ],
   },
   {
     img: "https://i.ibb.co/jZHG2xx/Vic.png",
